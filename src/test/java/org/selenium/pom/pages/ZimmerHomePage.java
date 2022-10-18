52d21bf28870b93dddfa9b06d6854442fb771e22
harjeetc <harjeet.chatha81@gmail.com>
2022-10-17 21:57:38 -0700

    first commit check

---
+++ src/test/java/org/selenium/pom/pages/ZimmerHomePage.java
@@ -0,0 +1,59 @@
+package org.selenium.pom.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.selenium.pom.base.BasePage;
+
+
+/*
+ * 
+ */
+public class ZimmerHomePage extends BasePage {
+	
+	//Headers section 
+   
+	@FindBy(xpath="(//a[contains(text(),'Find a Doctor')])[1]")
+	WebElement findADoclink1;
+	
+	@FindBy(xpath="(//a[contains(text(),'Careers')])[1]")
+	WebElement careers;
+	
+	@FindBy(xpath="(//a[contains(text(),'Investors')])[1]")
+	WebElement Investors;
+	
+	@FindBy(xpath="(//a[contains(text(),'ç Us')])[1]")
+	WebElement contact_Us;
+	
+	@FindBy(xpath="(//a[contains(text(),'Patients')])[1]")
+	WebElement patients;
+	
+	@FindBy(xpath="(//a[contains(text(),'Find a Rep')])[1]")
+	WebElement findARep;
+	
+	@FindBy(xpath="(//a[contains(text(),'US')])[1]")
+	WebElement US;
+	
+	
+	
+	//(//a[contains(text(),'US')])[1]
+	
+	
+	
+    public ZimmerHomePage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    
+    }
+
+  
+    public ZimmerHomePage load(){
+        load("/");
+        return this;
+    }
+    
+   public void clickFindDoc() {
+	   findADoclink1.click();
+   }
+}
---
+++ src/test/java/org/selenium/pom/tests/ZimmerHomePageTest.java
@@ -0,0 +1,23 @@
+package org.selenium.pom.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.selenium.pom.base.BaseTest;
+import org.selenium.pom.pages.HomePage;
+import org.selenium.pom.pages.ZimmerHomePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class ZimmerHomePageTest extends BaseTest {
+
+    @Test
+    public void homePageTest() throws InterruptedException {
+       ZimmerHomePage zimmerHomePage = new ZimmerHomePage(getDriver()).load();
+   
+       zimmerHomePage.clickFindDoc();
+       
+    }
+
+   
+}
