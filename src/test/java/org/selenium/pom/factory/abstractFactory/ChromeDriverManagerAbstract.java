f47a160a6f766f54d1df225d9f887084bbd008d2
Harjeet Chatha <harjeet.chatha@concentrix.com>
2023-04-04 11:45:56 -0700

    adding abstract refactoring webdriver manager

--- src/test/java/org/selenium/pom/factory/abstractFactory/ChromeDriverManagerAbstract.java
+++ src/test/java/org/selenium/pom/factory/abstractFactory/ChromeDriverManagerAbstract.java
@@ -1,23 +1,24 @@
 package org.selenium.pom.factory.abstractFactory;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.selenium.pom.factory.DriverManager;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ChromeDriverManagerAbstract extends DriverManagerAbstract {
 
-    @Override
-    protected void startDriver() {
-        WebDriverManager.chromedriver().cachePath("Drivers").setup();
-        ChromeOptions opt = new ChromeOptions();
-        opt.addArguments("--remote-allow-origins=*");
-        opt.addArguments("--headless");
-        opt.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        opt.addArguments("--no-sandbox"); // Bypass OS security model
-        driver = new ChromeDriver(opt);
-        driver.manage().window().maximize();
-        
-    }
+	@Override
+	protected void startDriver() {
+		// WebDriverManager.chromedriver().cachePath("Drivers").setup();
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions opt = new ChromeOptions();
+		opt.addArguments("--remote-allow-origins=*");
+		opt.addArguments("--headless=new");
+		opt.addArguments("--disable-dev-shm-usage"); // overcome limited resource
+		// problems
+		// opt.addArguments("--no-sandbox"); // Bypass OS security model
+		driver = new ChromeDriver(opt);
+		driver.manage().window().maximize();
+
+	}
 }
