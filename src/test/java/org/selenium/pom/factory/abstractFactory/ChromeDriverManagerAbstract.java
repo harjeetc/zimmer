ac87df6a887d022481d3c45b73e96da1caa1fc82
Harjeet Chatha <hchatha@pkglobal.com>
2023-04-04 18:01:51 -0700

    adding chrome options

--- src/test/java/org/selenium/pom/factory/abstractFactory/ChromeDriverManagerAbstract.java
+++ src/test/java/org/selenium/pom/factory/abstractFactory/ChromeDriverManagerAbstract.java
@@ -11,15 +11,27 @@ public class ChromeDriverManagerAbstract extends DriverManagerAbstract {
 	@Override
 	protected void startDriver() {
 		// WebDriverManager.chromedriver().cachePath("Drivers").setup();
+		System.out.println("*** startDriver for ChromeDriver ***");
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions opt = new ChromeOptions();
 		opt.addArguments("--remote-allow-origins=*");
-		opt.addArguments("--headless=new");
+		opt.addArguments("--headless");
 		opt.addArguments("--disable-dev-shm-usage"); // overcome limited resource
+		opt.addArguments("--window-size=1920x1200");
+		opt.addArguments("--screenshot");
+		opt.addArguments("--enable-screenshot-testing-with-mode");
+		opt.addArguments("--hide-scrollbars");
+		opt.addArguments("--disable-setuid-sandbox");
+		opt.addArguments("--no-sandbox");
+		opt.addArguments("--disable-gpu");
+		opt.addArguments("--disable-infobars");
+		opt.addArguments("--remote-debugging-port=9222");
+		opt.addArguments("--enable-automation");
+		opt.addArguments("--disable-notifications");
 		// problems
 		// opt.addArguments("--no-sandbox"); // Bypass OS security model
 		driver = new ChromeDriver(opt);
-		driver.manage().window().maximize();
+		//driver.manage().window().maximize();
 
 	}
    
